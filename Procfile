cdd8cb5515aa0f6ebba28b27bd459380a2ca47a1
Max Hoff <maxhoff@Maxs-MacBook-Pro.local>
2024-10-06 14:36:28 -0500

    project init

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python src/server/app.py
---
+++ src/server/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, redirect, url_for
+import random
+import string
+
+app = Flask(__name__)
+
+lobbies = {}
+
+def generate_code():
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/create', methods=['POST'])
+def create_lobby():
+    code = generate_code()
+    lobbies[code] = {"players": []}
+    return redirect(url_for('lobby', code=code))
+
+@app.route('/lobby/<code>')
+def lobby(code):
+    if code in lobbies:
+        return render_template('lobby.html', code=code)
+    else:
+        return "Lobby not found", 404
+
+@app.route('/join', methods=['POST'])
+def join_lobby():
+    code = request.form['code']
+    if code in lobbies:
+        return redirect(url_for('lobby', code=code))
+    else:
+        return "Invalid code", 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
