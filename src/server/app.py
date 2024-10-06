5ae8cb55aacc7b245d4b26e06d7b8a29354cc906
Max Hoff <maxhoff@Maxs-MacBook-Pro.local>
2024-10-06 18:37:45 -0500

    imports updated

--- src/server/app.py
+++ src/server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, jsonify
 import random
 import string
 import os
